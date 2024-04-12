4fe27d827f269b97fcb60854badb99b818758456
Luc-kn <56149695+Luc-kn@users.noreply.github.com>
2024-04-12 10:31:40 +0200

    Revert "15 crud van advies"

--- ZuydFit/DAL.cs
+++ ZuydFit/DAL.cs
@@ -12,13 +12,6 @@ namespace ZuydFit
     public class DAL
     {
         public List <Activity> activities = new List <Activity> ();
-        public List<Advice> Advices { get; set; }
-        public string connectionString = "Data Source=.;Initial Catalog=ZuydFit;Integrated Security=True;Encrypt=False";
-
-
-
-
-
        
         public string connectionString = "Data Source=.;Initial Catalog=ZuydFit;Integrated Security=True;Trust Server Certificate=True";
         
@@ -38,47 +31,6 @@ namespace ZuydFit
                         "VALUES (@Name, @Description, @Duration, @Sets)";
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                      
-              public void CreateAdvice(Advice advice)
-                        connection.Open();
-                        string sql = "INSERT INTO Advice (Id, Description) " +
-                            "VALUES (@Id, @Description) ";
-                        using (SqlCommand command = new SqlCommand(sql, connection))
-                        {
-                            command.Parameters.AddWithValue("Id", @advice.Id);
-                            command.Parameters.AddWithValue("Description", @advice.Description);
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                }
-                catch (Exception ex) { throw ex; }
-            }
-            public void ReadAdvice(Advice advice)
-            {
-                try { 
-                using (SqlConnection connection = new SqlConnection())
-                {
-                    using (SqlCommand command = new SqlCommand())
-                    {
-                        connection.ConnectionString = connectionString;
-                        connection.Open();
-                        command.Connection = connection;
-                        command.CommandText = "SELECT Id, Description " +
-                            "FROM Advice ORDER BY ID ";
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Advices.Add(new Advice (reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString()));
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex) { throw ex ; }
-        }
-            public void UpdateAdvice(Advice advice)
-
                         command.Parameters.AddWithValue("Name", activity.Name);
                         command.Parameters.AddWithValue("Description", activity.Description);
                         command.Parameters.AddWithValue("Duration", activity.Duration);
@@ -98,7 +50,6 @@ namespace ZuydFit
         {
 
             using (SqlConnection connection = new SqlConnection())
-
             {
                 using (SqlCommand command = new SqlCommand())
                 {
@@ -109,40 +60,6 @@ namespace ZuydFit
                         "FROM Product ORDER BY ID ";
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        connection.Open();
-                        string sql = "UPDATE Advice SET Id = @Id, Description = @description, " +
-                            "WHERE Id = @id";
-                        using (SqlCommand command = new SqlCommand(sql, connection))
-                        {
-                            command.Parameters.AddWithValue("Id", advice.Id);
-                            command.Parameters.AddWithValue("Description", advice.Description);
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                }
-                catch (Exception ex) { throw ex; }
-            }
-            public void DeleteAdvice(Advice advice)
-            {
-                try { 
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        connection.Open();
-                        string sql = "DELETE FROM Advice WHERE Id = @Id";
-                        using (SqlCommand command = new SqlCommand(sql, connection))
-                        {
-                            command.Parameters.AddWithValue("@Id", advice.Id);
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                }
-                catch (Exception ex) { throw ex; }
-        }
-
-
-
-        
-
                         while (reader.Read())
                         {
                             //de uitroeptekens staan ervoor omdat de copiler denkt dat het een null waarde kan hebben terwijl dat zeker niet zo is.
@@ -189,7 +106,6 @@ namespace ZuydFit
                 }
             }
         }*/
-
 
 
 
