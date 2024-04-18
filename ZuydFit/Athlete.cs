332b4b8f29d686a06577d88e3b4e01358bc6d78b
Luc-kn <knoppen.luc@gmail.com>
2024-04-18 10:33:10 +0200

    updates

--- ZuydFit/Athlete.cs
+++ ZuydFit/Athlete.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZuydFit.ZuydFit;
 
 namespace ZuydFit
 {
@@ -12,7 +13,7 @@ namespace ZuydFit
         public List<Advice> Advices { get; set; }
 
 
-        public Athlete(int id, string firstName, string lastName, int personalNumber, List<Goal> goals, List<Advice> advices) : base(id, firstName,lastName, personalNumber)
+        public Athlete(int id, string firstName, string lastName, int personalNumber, List<Goal> goals, List<Advice> advices, string password) : base(id, firstName,lastName, personalNumber, password)
         {
             Goals = goals;
             Advices = advices;
